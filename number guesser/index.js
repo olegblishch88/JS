0351c1ecdd5c83d23bb8e0ae7f404205614b4994
Oleg Blishch <32680137+olegblishch88@users.noreply.github.com>
2019-01-17 16:04:43 +0200

    Guess who's back?

---
+++ number guesser/index.js
@@ -0,0 +1,60 @@
+let min = 1, 
+    max = 10,
+    winningNum = Math.floor(Math.random() * 10),
+    guessesLeft = 3;
+
+console.log(winningNum);
+
+const game = document.querySelector('#game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessBtn = document.querySelector('#guess-btn'),
+    guessInput = document.querySelector('#guess-input'),
+    message = document.querySelector('.message');
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+game.addEventListener('mousedown', e => {
+    if(e.target.className === 'play-again'){
+        window.location.reload();
+    }
+})
+
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+    if(isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    }
+    if(guess === winningNum){
+        gameOver(true, `${winningNum} is correct, YOU WIN!`);
+    }else{
+        guessesLeft -= 1;
+        if(guessesLeft === 0){
+           gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+        }else{
+            guessInput.style.borderColor = 'green';
+            guessInput.value = '';
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left.`, 'red');
+        }
+    }
+}); 
+
+const gameOver = (won, msg) => {
+    let color;
+    won === true ? color = 'green' : color = 'red';
+    guessInput.disabled = true;
+    guessInput.style.borderColor = color;
+    message.style.color = color;
+    setMessage(msg);
+
+    guessBtn.value = 'Play again';
+    guessBtn.className += 'play-again';
+}
+
+const setMessage = (msg, color) => {
+    message.style.color = color;
+    message.textContent = msg;
+}
+
+    
