a3385036a52317517894b56da8e8bac0d59219e3
Oleg Blishch <32680137+olegblishch88@users.noreply.github.com>
2019-01-18 20:12:27 +0200

    chuck norris built the hospital that he was born in

---
+++ chuck-norris jokes/index.js
@@ -0,0 +1,43 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e){
+    const number = document.querySelector('input[type="number"]').value;
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+    xhr.onload = function(){
+        if(this.status === 200){
+            const res = JSON.parse(this.responseText);
+            let output = '';
+            if(res.type === 'success'){
+                res.value.forEach(function(joke){
+                    output += `<li>${joke.joke}</li>`
+                })
+            }else{
+                output += '<li>Something went wrong...</li>';
+            }
+            document.querySelector('.jokes').innerHTML = output;
+        }
+    }
+    xhr.send()
+    e.preventDefault();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
